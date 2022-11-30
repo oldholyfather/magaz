bf39af92cce1dfb1a6c5ad11f115f060679f658e
Alina <Alina@DESKTOP-KM81G95>
2022-12-01 01:27:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Controller.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Controller
+    {
+
+        public static List<Goods> Goods = new List<Goods>();
+        public void AddGood (string title, float price, float sale)
+        {
+            Goods good = new Goods()
+            {
+                Title = title,
+                Price = price,
+                Sale = sale,
+                
+            };
+            good.Price = good.Price - good.Sale;
+            Goods.Add(good);
+        }
+        public string GetGoods()
+        {
+            int i = 1;
+            string text = "";
+            foreach (var item in Goods)
+            {
+
+                text += $"номер товара - {i}, {item.Title}, цена со скидкой- {item.Price}\n";
+                i++;
+            }
+            return text;
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Введите номер товара по порядку, который хотите удалить");
+            int nomer_tovara = Convert.ToInt32(Console.ReadLine());
+
+            Goods.RemoveAt(nomer_tovara-1);
+        }
+
+        public void SaveList()
+        {
+            var json = JsonSerializer.Serialize<List<Goods>>(Goods);
+
+            File.WriteAllText("list.json", json);
+        }
+        public void OpenList()
+        {
+            if (!File.Exists("list.json"))
+                return;
+
+            var json = File.ReadAllText("list.json");
+
+            Goods = JsonSerializer.Deserialize<List<Goods>>(json);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp1;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Выберите команду");
+        Console.WriteLine("доступный команды: добавить, список, удалить, сохранить, загрузить");
+        Console.WriteLine("Примеры написания команд: \n добавить яблоко 150(цена) 10(скидка) \n удалить, ENTER, 1 \n список \n сохранить \n загрузить");
+
+        while (true)
+        {
+            var  command = Console.ReadLine().ToLower().Split(' ');
+            Controller s = new Controller();
+            switch (command[0])
+            {
+
+                case "добавить":
+                    s.AddGood(command[1], float.Parse(command[2]), float.Parse(command[3]));
+                    break;
+                case "список":
+                    Console.WriteLine(s.GetGoods());
+                    break;
+                case "сохранить":
+                    s.SaveList();
+                    break;
+                case "загрузить":
+                    s.OpenList();
+                    break;
+                case "удалить":
+                    s.Delete();
+                    break;
+                default:
+                    Console.WriteLine("Ошибка в команде");
+                    break;
+            }
+        }
+
+
+    }
+
+}
